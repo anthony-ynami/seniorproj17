d53f61e2b5ced335361f5925c5e7e2b47f0e1c61
anthony-ynami <anthony.ynami@nyu.edu>
2017-10-18 15:50:27 -0400

    rita test

---
+++ ritaTest/sketch.js
@@ -0,0 +1,35 @@
+var input;
+var button;
+var lexicon;
+
+function setup() {
+    noCanvas();
+    lexicon = new RiLexicon();
+        
+    input = createInput("It was a dark and stormy night.");
+    button = createButton('submit');
+    input.changed(processRita);
+    input.size(200);
+    button.mousePressed(processRita);
+}
+
+function processRita() {
+    var s = input.value();
+    var rs = new RiString(s);
+    var words = rs.words();
+    var pos = rs.pos();
+    console.log(words);
+    console.log(pos);
+    
+    var output = '';
+    for (var i = 0; i < words.length;i++){
+        if(/nn.*/.test(pos[i])) {
+            output += lexicon.randomWord('nn')
+        } else{
+            output += words[i];
+        }
+        
+        output += ' ';
+    }
+      createP(output);
+}
