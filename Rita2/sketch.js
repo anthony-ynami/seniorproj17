12717cf75d5a5352b3bf7c348ea1dc5a5cb91dd7
anthony-ynami <anthony.ynami@nyu.edu>
2017-11-21 14:31:58 -0500

    rita2

---
+++ Rita2/sketch.js
@@ -0,0 +1,41 @@
+var rg
+
+function setup() {
+    noCanvas();
+    lexicon = new RiLexicon();
+  	rg = new RiGrammar();
+
+
+
+  	
+
+ 	rg.addrule('<start>','the <N> meows',1);
+ 	rg.addrule('<N>','cat | unicorn',5);
+ 	rg.addrule('<N>','dog');\
+ 	for (var i =0; i < 100; i++) {
+ 		var result = rg.expand();
+ 		console.log(result);
+ 	}
+ 	
+}
+
+function processRita() {
+    var s = input.value();
+    var rs = new RiString(s);
+    var words = rs.words();
+    var pos = rs.pos();
+    console.log(words);
+    console.log(pos);
+    
+    var output = '';
+    for (var i = 0; i < words.length;i++){
+        if(/nn.*/.test(pos[i])) {
+            output += lexicon.randomWord('nn')
+        } else{
+            output += words[i];
+        }
+        
+        output += ' ';
+    }
+      createP(output);
+}
